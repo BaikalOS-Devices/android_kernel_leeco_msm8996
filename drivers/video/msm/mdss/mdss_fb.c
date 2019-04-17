72d708179fb4adb5f6070aeb37a03caab53cfb54
sdv22 <sdv22@live.ru>
2019-04-17 07:07:27 +0300

    Increase BW boost on frame to x2

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -4909,10 +4909,10 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		ret = mdss_fb_mode_switch(mfd, dsi_mode);
 		break;
 	case MSMFB_ATOMIC_COMMIT:
-        if( boost_on_frame ) {
-            cpu_input_boost_kick_max(16);
+        	if( boost_on_frame ) {
+	            cpu_input_boost_kick_max(32);
 		    devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
-        }
+        	}
 		ret = mdss_fb_atomic_commit_ioctl(info, argp, file);
 		break;
 
