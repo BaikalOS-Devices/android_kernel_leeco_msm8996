94d95c7dc78511cc1a91df9185171f3f7d6c8e0b
Sergey Vasiliev <sdv22@live.ru>
2020-08-30 21:55:59 +0300

    Increase appstart boost to 3s

--- kernel/fork.c
+++ kernel/fork.c
@@ -1696,11 +1696,10 @@ long do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
-	/* Boost CPU to the max for 1250 ms when userspace launches an app */
+	/* Boost CPU to the max for 3000 ms when userspace launches an app */
 	if (is_zygote_pid(current->pid)) {
-		cpu_input_boost_kick_max(500);
-		//devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 500);
-		devfreq_boost_kick_max_all(500);
+		cpu_input_boost_kick_max(3000);
+		devfreq_boost_kick_max_all(3000);
 	}
 	/*
 	 * Determine whether and which event to report to ptracer.  When
